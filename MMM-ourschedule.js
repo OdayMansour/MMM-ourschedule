c927d814bbdec58ce978bd8b14d1279a5cd91fbe
Oday Mansour <odaym@nsour.net>
2018-07-27 19:30:10 +0000

    Now fetching state, days, from schedule api

--- MMM-ourschedule.js
+++ MMM-ourschedule.js
@@ -11,33 +11,57 @@ Module.register("MMM-ourschedule",{
 
 	// Default module config.
 	defaults: {
-		text: "Hello World!"
 	},
 
 	start: function() {
-		this.filler = "Filler text"
-		this.config.text = "Hello there"
+		this.globals = {}
+		this.globals.year = new Date().getFullYear()
+		this.globals.month = new Date().getMonth() + 1 // getMonth() -> Jan = 0, Dec = 11
+
+		this.state = []
+		this.days = []
+
+		this.ready = {}
+		this.ready['state'] = false
+		this.ready['days'] = false
+
+		this.filler = 'Requesting...'
 
 		var self = this
 
-		var request = new XMLHttpRequest();
-		request.open('GET', 'http://oday.nyc:1616/text/nothing', true);
-		request.onload = function () {
-			self.processAnswer(this.responseText)
+		var request_state = new XMLHttpRequest();
+		request_state.open('GET', 'http://oday.nyc:1616/state/year/' + this.globals.year + '/month/' + this.globals.month, true);
+		request_state.onload = function () {
+			self.state = JSON.parse(this.responseText)
+			console.log(self.state)
+			self.reportReadiness('state')
 		}
-		request.send();
+		request_state.send();
+
+		var request_days = new XMLHttpRequest();
+		request_days.open('GET', 'http://oday.nyc:1616/days/year/' + this.globals.year + '/month/' + this.globals.month, true);
+		request_days.onload = function () {
+			self.days = JSON.parse(this.responseText)
+			console.log(self.days)
+			self.reportReadiness('days')
+		}
+		request_days.send();
+	},
+
+	reportReadiness: function(item) {
+		this.ready[item] = true
+
+		if (this.ready['state'] && this.ready['days']) {
+			this.filler = 'Ready'
+		}
+
+		this.updateDom()
 	},
 
 	getDom: function() {
 		var calendar = document.createElement("div")
-
-		calendar.innerHTML = this.filler
-
+		calendar.innerHTML = this.filler 
 		return calendar
-	},
-
-	processAnswer: function(response) {
-		this.filler = response
-		this.updateDom()
 	}
+
 });
